909b030d32fd0a1386f0d19f0d9c4b6c2315a582
danalvin <mungaiwaituika@gmail.com>
2018-08-16 11:59:37 +0300

    file modified

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,7 +5,7 @@ import { Directive,ElementRef} from '@angular/core';
 })
 export class HighlightDirective {
   constructor(private elem:ElementRef) { 
-    this.elem.nativeElement.style.backgroundColor="darkgrey"
+  
 
   }
 
